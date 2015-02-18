4a42a3fecb0dc42aa9cbef315f7688c9f7bd0c51
John Pinkerton <jpinkerton88@gmail.com>
2015-02-18 15:44:11 -0500

    add slideshow

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 
-  $.backstretch("assets/images/your-iphone.jpg");
+  images = ["assets/images/your-iphone.jpg",
+            "assets/images/her-ipad.jpg",
+            "assets/images/his-ipad.jpg",
+            "assets/images/their-iphones.jpg"];
+
+  phrases = ["your iphone porn free",
+             "her ipad porn free",
+             "his ipad porn free",
+             "their iphones porn free"];
+
+  $(window).on("backstretch.before", function (e, instance, index) {
+    if (index == 4) {
+      index -= 4;
+    }
+    $('h1').text(phrases[index]);
+  });
+
+  $.backstretch(images, { fade: 500 });
 
 });
