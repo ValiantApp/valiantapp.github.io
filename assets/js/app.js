f6faa28e5914ba14e855a0fd723215424e536674
Sam Claridge <st.claridge@gmail.com>
2015-02-18 15:59:01 -0500

    update order of slideshow

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 
   images = ["assets/images/your-iphone.jpg",
             "assets/images/her-ipad.jpg",
-            "assets/images/his-ipad.jpg",
-            "assets/images/their-iphones.jpg"];
+            "assets/images/their-iphones.jpg",
+            "assets/images/his-ipad.jpg"];
 
   phrases = ["your iphone porn free",
              "her ipad porn free",
-             "his ipad porn free",
-             "their iphones porn free"];
+             "their iphones porn free",
+             "his ipad porn free"];
 
   $(window).on("backstretch.before", function (e, instance, index) {
     if (index == 4) {
