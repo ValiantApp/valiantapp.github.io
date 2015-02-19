9ffef5a1eb6666b1c13471e49427b5c2ff3dda14
Sam Claridge <st.claridge@gmail.com>
2015-02-19 09:32:29 -0500

    move "your iphone" to last

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
-  images = ["assets/images/your-iphone.jpg",
-            "assets/images/her-ipad.jpg",
+  images = ["assets/images/her-ipad.jpg",
             "assets/images/their-iphones.jpg",
-            "assets/images/his-ipad.jpg"];
+            "assets/images/his-ipad.jpg",
+            "assets/images/your-iphone.jpg"];
 
-  phrases = ["your iphone porn free",
-             "her ipad porn free",
+  phrases = ["her ipad porn free",
              "their iphones porn free",
-             "his ipad porn free"];
+             "his ipad porn free",
+             "your iphone porn free"];
 
   $(window).on("backstretch.before", function (e, instance, index) {
     if (index == 4) {
